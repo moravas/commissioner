012158dc97ef508a422186ae867e3bebf7f2ff2a
Norbert Toth <norbert.toth.ees@gmail.com>
2016-09-20 13:09:07 +0200

    rework schema for the more intuitive use

--- schema.js
+++ schema.js
@@ -1,180 +1,49 @@
 var schema = {
-    "standalone": [
-        {
-            "-name": "network",
-            "ipproperty": [
-                { "-name": "IPAddress" },
-                { "-name": "networkMask" },
-                { "-name": "defaultGateway" }
-            ]
+    "standalone": {
+        "network": {
+            "IPAddress": "ipv4",
+            "NetworkMask": "ipv4",
+            "DefaultGateway": "ipv4"
         },
-        {
-            "-name": "sysconfig",
-            "integralproperty": [
-                {
-                    "-name": "hwInputNumber",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "hwOutputNumber",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                }
-            ],
-            "stringproperty": {
-                "-name": "cik",
-                "-size": "40"
-            }
+        "sysconfig": {
+            "hwInputNumber": "int<2>",
+            "hwOutputNumber": "int<2>",
+            "cik": "string<40>"
         }
-    ],
-    "enumeration": [
-        {
-            "-name": "input",
-            "integralproperty": {
-                "-name": "trigger",
-                "-queryas": "unsigned char",
-                "-size": "1",
-                "-values": "BothEdges,RisingEdge,DeferredBothEdges,DeferredRisingEdge"
-            },
-            "stringproperty": {
-                "-name": "name",
-                "-size": "16"
-            }
+    },
+    "enumeration": {
+        "input": {
+            "trigger": "enum<BothEdges,RisingEdge,DeferredBothEdges,DeferredRisingEdge>",
+            "name": "string<16>"
         },
-        {
-            "-name": "output",
-            "integralproperty": [
-                {
-                    "-name": "address",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "timeoutOFF",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "timeoutON",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                }
-            ],
-            "stringproperty": {
-                "-name": "name",
-                "-size": "16"
-            },
-            "variadicproperty": {
-                "-name": "inputs",
-                "-queryas": "std::vector<unsigned short>",
-                "-storagetype": "unsigned short",
-                "-splitup": ";"
-            }
+        "output": {
+            "address": "int<2>",
+            "timeoutOFF": "int<2>",
+            "timeoutON": "int<2>",
+            "name": "string<16>",
+            "inputs": "array[int<2>]"
         },
-        {
-            "-name": "tristateoutput",
-            "integralproperty": [
-                {
-                    "-name": "addresslow",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "addresshigh",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "timeoutOFF",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "timeoutON",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                }
-            ],
-            "stringproperty": {
-                "-name": "name",
-                "-size": "16"
-            },
-            "variadicproperty": [
-                {
-                    "-name": "inputsUpDown",
-                    "-queryas": "std::vector<unsigned short>",
-                    "-storagetype": "unsigned short",
-                    "-splitup": ";"
-                },
-                {
-                    "-name": "inputsDown",
-                    "-queryas": "std::vector<unsigned short>",
-                    "-storagetype": "unsigned short",
-                    "-splitup": ";"
-                },
-                {
-                    "-name": "inputsUp",
-                    "-queryas": "std::vector<unsigned short>",
-                    "-storagetype": "unsigned short",
-                    "-splitup": ";"
-                }
-            ]
+        "tristateoutput": {
+            "addresslow": "int<2>",
+            "addresshigh": "int<2>",
+            "timeoutOFF": "int<2>",
+            "timeoutON": "int<2>",
+            "name": "string<16>",
+            "inputsUpDown": "array[int<2>]",
+            "inputsDown": "array[int<2>]",
+            "inputsUp": "array[int<2>]"
         },
-        {
-            "-name": "irrigation",
-            "integralproperty": [
-                {
-                    "-name": "area",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "input",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "upTime",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "startTime",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "mode",
-                    "-queryas": "unsigned char",
-                    "-size": "1",
-                    "-values": "Normal,Grown"
-                },
-                {
-                    "-name": "offsetTime",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "dailyPrecipitation",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "unitPrecipitation",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                },
-                {
-                    "-name": "repeatCount",
-                    "-queryas": "unsigned short",
-                    "-size": "2"
-                }
-            ],
-            "stringproperty": {
-                "-name": "name",
-                "-size": "16"
-            }
+        "irrigation": {
+            "area": "int<2>",
+            "input": "int<2>",
+            "upTime": "int<2>",
+            "startTime": "int<2>",
+            "mode": "enum<Normal,Grown>",
+            "offsetTime": "int<2>",
+            "dailyPrecipitation": "int<2>",
+            "unitPrecipitation": "int<2>",
+            "repeatCount": "int<2>",
+            "name": "string<16>"
         }
-    ]
+    }
 }
