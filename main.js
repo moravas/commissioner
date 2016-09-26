5d2a07a70bcd996506e5b30107c5733a24b20adc
moravas <moravas8051@gmail.com>
2016-09-26 20:11:09 +0200

    Add default table generation

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
+function generateInputField(fieldType) {
+    if (fieldType.startsWith("array")) {return null;}
+
+    var match = /(.*)<(\d{1,5})>/.exec(fieldType);
+    var inputType = (match == null ? fieldType : match[1]);
+    var valueSet = (match == null ? 0 : match[2]);
+    switch(inputType) {
+        case "ipv4": {
+            var input = document.createElement("input");
+            input.type = "text";
+            input.placeholder = "XXX.XXX.XXX.XXX";
+            return input;
+        }
+        case "int": {
+            var input = document.createElement("input");
+            input.type = "number";
+            input.min = 0;
+            input.max = Math.pow(2, valueSet * 8) - 1;
+            return input;
+        }
+        case "string": {
+            var input = document.createElement("input");
+            input.type = "text";
+            input.maxLength = valueSet;
+            return input;
+        }
+    }
+}
+
+function getNamedTable(name) {
+    var table = document.createElement("table");
+    var row = table.insertRow(-1);
+    var cell = row.insertCell(-1);
+    cell.innerHTML = name;
+    return table;
+}
+
+function createEnumerationTables() {
+    Object.keys(schema.enumeration).forEach(function (node) {
+        var table = getNamedTable(node);
+        var row = table.insertRow(-1);
+        Object.keys(schema.enumeration[node]).forEach(function (key) {
+            var cell = row.insertCell(-1);
+            cell.innerHTML = key;
+        })
+
+        document.getElementById("project").appendChild(table);
+    })
+}
+
+function createStandalonTables() {
+    Object.keys(schema.standalone).forEach(function (node) {
+        var table = getNamedTable(node);
+        Object.keys(schema.standalone[node]).forEach(function (key) {
+            var row = table.insertRow(-1);
+            var cell = row.insertCell(-1);
+            cell.innerHTML = key;
+            cell = row.insertCell(-1);
+            cell.contentEditable = true;
+            cell.appendChild(generateInputField(schema.standalone[node][key]));
+        })
+
+        document.getElementById("project").appendChild(table);
+    })
+}
+
 angular.module("app", [])
     .controller("ctrl", ["$scope", function ($scope) {
+        createStandalonTables();
+        createEnumerationTables();
     }])
