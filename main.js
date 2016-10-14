ad9a28bcfce495f75104840c5713cf33d703d297
Norbert Toth <norbert.toth.ees@gmail.com>
2016-10-14 13:13:14 +0200

    views have been added

--- main.js
+++ main.js
@@ -1,45 +1,30 @@
-function generateInputField(fieldType) {
-    if (fieldType.startsWith("array")) { return null; }
+var app = angular.module("app", ["ngRoute"]);
 
-    var match = /(.*)<(.*)>/.exec(fieldType);
-    var inputType = (match == null ? fieldType : match[1]);
-    var valueSet = (match == null ? 0 : match[2]);
-    switch (inputType) {
-        case "ipv4": {
-            var input = document.createElement("input");
-            input.type = "text";
-            input.placeholder = "XXX.XXX.XXX.XXX";
-            return input;
-        }
-        case "int": {
-            var input = document.createElement("input");
-            input.type = "number";
-            input.min = 0;
-            input.max = Math.pow(2, valueSet * 8) - 1;
-            return input;
-        }
-        case "string": {
-            var input = document.createElement("input");
-            input.type = "text";
-            input.maxLength = valueSet;
-            return input;
-        }
-        case "enum": {
-            var input = document.createElement("select");
-            var options = valueSet.split(",");
-            for (index = 0; index < options.length; index++) {
-                var option = document.createElement("option");
-                option.text = options[index];
-                option.value = index;
-                input.appendChild(option);
-            }
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/new', {
+            templateUrl: 'view/config.html',
+            controller: 'configController'
+        }).
+        when('/debug', {
+            templateUrl: 'view/debug.html',
+            controller: 'debugController'
+        }).
+        otherwise({
+            template: "<h1>404 Not found</h1>"
+        });
+}]);
 
-            return input;
-        }
-    }
-}
+app.controller("configController", ["$scope", function ($scope) {
+    $scope.schema = schema;
+    $scope.enumerate = function (n) {
+        return new Array(n);
+    };
+}]);
 
-var app = angular.module("app", []);
+app.controller("debugController", ["$scope", function ($scope) {
+}]);
+
 app.filter("metaFilter", function () {
     return function (partition) {
         var res = Object.assign({}, partition);
@@ -50,11 +35,3 @@ app.filter("metaFilter", function () {
         return res;
     };
 });
-
-app.controller("ctrl", ["$scope", function ($scope) {
-    $scope.schema = schema;
-    $scope.generateInputField = generateInputField;
-    $scope.enumerate = function (n) {
-        return new Array(n);
-    };
-}]);
