831006116889f51053efd07f20ab387725c2b03f
Norbert Toth <norbert.toth.ees@gmail.com>
2016-11-30 14:15:27 +0100

    Base concept of navigating between config tables has been added

--- main.js
+++ main.js
@@ -1,28 +1,36 @@
+function tabChanged(tableName) {
+    var toHide = document.getElementsByClassName('partitionTable show');
+    if (toHide[0] != null) {
+        toHide[0].className = toHide[0].className.replace('show', 'hidden');
+    }
+    var toVisible = document.getElementById(tableName);
+    if (toVisible != null) {
+        toVisible.className = toVisible.className.replace('hidden', 'show');
+    }
+}
+
 var app = angular.module("app", ["ngRoute"]);
 
 app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.
-        when('/new', {
+    Object.keys(project).forEach(function (operation) {
+        $routeProvider.when('/' + operation, {
             templateUrl: 'view/config.html',
             controller: 'configController'
-        }).
-        when('/debug', {
-            templateUrl: 'view/debug.html',
-            controller: 'debugController'
-        }).
-        otherwise({
-            template: "<h1>404 Not found</h1>"
-        });
+        })
+    })
+
+    $routeProvider.otherwise({
+        template: "<h1>404 Not found</h1>"
+    });
 }]);
 
 app.controller("configController", ["$scope", function ($scope) {
     $scope.schema = schema;
+    $scope.project = project;
+    $scope.tabChanged = tabChanged;
     $scope.enumerate = function (n) {
         return new Array(n);
     };
-}]);
-
-app.controller("debugController", ["$scope", function ($scope) {
 }]);
 
 app.filter("metaFilter", function () {
---
+++ project.js
@@ -0,0 +1,9 @@
+var project = {
+    New: function () { },
+    Open: function () { },
+    Save: function () { },
+    Compile: function () { },
+    Upload: function () { },
+    Download: function () { },
+    Debug: function () { }
+}
