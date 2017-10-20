4eec02ec7d512b3446fbfa5e8fff86a80140c3d6
tnorbert <tnorbert@deverto.com>
2017-10-20 10:37:03 +0200

    Add title to the chart

--- chart.jsx
+++ chart.jsx
@@ -8,19 +8,22 @@ import { LineChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Line } from 'r
 export default class Chart extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {chartData: []};
+        this.state = { chartData: [] };
 
         update();
     }
 
     render() {
         return (
-            <LineChart width={600} height={300} data={this.state.chartData}>
-                <XAxis dataKey="timeStamp" />
-                <YAxis unit=" km" />
-                <CartesianGrid strokeDasharray="3 3" />
-                <Line type="monotone" dataKey="va" stroke="#8884d8" isAnimationActive={false} />
-            </LineChart>
+            <div>
+                <h3>{this.props.title}</h3>
+                <LineChart width={600} height={300} data={this.state.chartData}>
+                    <XAxis dataKey="timeStamp" />
+                    <YAxis unit=" km" />
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <Line type="monotone" dataKey="va" stroke="#8884d8" isAnimationActive={false} />
+                </LineChart>
+            </div>
         );
     }
 
